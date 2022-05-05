53c4bae28b052d2a3814c66b1fd9f148fc93607d
waltergu <waltergu1989@gmail.com>
2022-05-05 20:14:57 +0800

    Fix bugs.

--- src/SysImageGenerator.jl
+++ src/SysImageGenerator.jl
@@ -2,33 +2,42 @@ module SysImageGenerator
 
 using PackageCompiler: create_sysimage
 using CompileBot: pathof_noload
+using Pkg
 
 import IJulia: installkernel
 
 export sysimage, extract, installkernel
 
 const supported_packages = ["QuantumLattices", "ExactDiagonalization", "TightBindingApproximation", "SpinWaveTheory", "MagnonPhonons"]
+const default_sysimage = "sys_QuantumManyBody.so"
 
 """
-    sysimage(name::String, packages::Vector{String}=supported_packages; plot::Bool=true, symbolic::Bool=true)
+    sysimage(name::String=default_sysimage, packages::Vector{String}=copy(supported_packages); plot::Bool=true, symbolic::Bool=true)
 
+Create a sysimage.
 """
-function sysimage(name::String, packages::Vector{String}=supported_packages; plot::Bool=true, symbolic::Bool=true)
+function sysimage(name::String=default_sysimage, packages::Vector{String}=copy(supported_packages); plot::Bool=true, symbolic::Bool=true)
     path = dirname(dirname(pathof_noload("SysImageGenerator")))
+    Pkg.activate(path)
     files = String[]
     for package in packages
         @assert package∈supported_packages "sysimage error: not supported package ($package)."
         push!(files, "$path/scripts/precompile_$package.jl")
     end
-    accessories = String[]
-    plot && push!(accessories, "Plots")
-    symbolic && push!(accessories, "SymPy")
+    plot && push!(packages, "Plots")
+    symbolic && push!(packages, "SymPy")
     create_sysimage(packages;
         sysimage_path="$path/sysimages/$name",
         precompile_execution_file=files
     )
+    Pkg.activate()
 end
 
+"""
+    extract(package_name::String)
+
+Extract the test sets of a package and run them.
+"""
 function extract(package_name::String)
     package_rootpath = dirname(dirname(pathof_noload(package_name)))
     runtestpath = "$package_rootpath/test/runtests.jl"
@@ -40,7 +49,12 @@ function extract(package_name::String)
     Core.eval(Main, snoop_script)
 end
 
-function installkernel(sysimage::String, kernel::String="Julia-QuantumManyBody")
+"""
+    installkernel(sysimage::String=default_sysimage, kernel::String="Julia-QuantumManyBody")
+
+Install a Jupyter notebook kernel with the specified sysimage.
+"""
+function installkernel(sysimage::String=default_sysimage, kernel::String="Julia-QuantumManyBody")
     path = dirname(dirname(pathof_noload("SysImageGenerator")))
     sysimage = "$path/sysimages/$sysimage"
     installkernel(kernel, "--project=@.", "--sysimage=$(sysimage)")
